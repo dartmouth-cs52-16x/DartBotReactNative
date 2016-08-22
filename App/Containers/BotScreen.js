79772afe2fdeaa49b176ece51a4fe99aaee48c83
AhsanAzim <ahsan-azim@live.com>
2016-08-22 00:21:54 -0400

    iframe embedded

--- App/Containers/BotScreen.js
+++ App/Containers/BotScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { ScrollView, Text } from 'react-native'
+import { WebView } from 'react-native';
+import { ScrollView, Text, Dimensions } from 'react-native'
 import { connect } from 'react-redux'
 import Actions from '../Actions/Creators'
 import { Actions as NavigationActions } from 'react-native-router-flux'
@@ -16,9 +17,10 @@ class BotScreen extends React.Component {
 
   render () {
     return (
-      <ScrollView style={styles.container}>
-        <Text>BotScreen Container</Text>
-      </ScrollView>
+      <WebView
+        source={{uri: 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdartbot&tabs=messages&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1780533645501545'}}
+        style={{ marginTop: 75, marginLeft: 40, height: Dimensions.get('window').height, width: Dimensions.get('window').width  }}
+      />
     )
   }
 }
